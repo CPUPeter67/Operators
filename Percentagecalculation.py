970b9ef9c66eef15d5ef65bf003a2a30eb1c91a8
CPUPetet <c.nwodo@abujaprep.com>
2026-03-15 17:34:02 +0100

    percentagecalc

---
+++ Percentagecalculation.py
@@ -0,0 +1,10 @@
+Maths =int(input("Please enter the marks for Maths:"))
+English =int(input("Please enter the marks for English:"))
+Science =int(input("Please enter the marks for Science:"))
+PE =int(input("Please enter the mars for P.E:"))
+
+sum = Maths+English+Science+PE
+print("Sum of all marks: ", sum)
+
+average = (sum/400)*100
+print("Average percentage: ", average)
